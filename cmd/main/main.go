568ad317a26a0bf70ef77f4e99764f26788c50ac
Kosk0l <kosk0l@yandex.ru>
2025-12-24 18:32:12 +0300

    create App struct

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,42 +1,11 @@
 package main
 
 import (
-	log "log"
-	os "os"
-
-	"github.com/Kosk0l/TgBotConverter/intrernal/handlers"
-	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
+	"github.com/Kosk0l/TgBotConverter/config"
+	"github.com/Kosk0l/TgBotConverter/intrernal/app"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	
-
-	token := os.Getenv("TELEGRAM_TOKEN")
-	if token == "" {
-		log.Fatal("TELEGRAM_TOKEN is not set")
-	}
-
-	// Создаём бота
-	bot, err := telegram.NewBotAPI(token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bot.Debug = true // можно отключить потом
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	// Настраиваем получение апдейтов
-	u := telegram.NewUpdate(0)
-	u.Timeout = 30
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		handlers.HandleUpdate(bot, update)
-	}
+	cfg := config.Load()
+	app.NewApp(cfg)
 }
--- intrernal/app/app.go
+++ intrernal/app/app.go
@@ -1,1 +1,34 @@
 package app // Связка компонентов
+
+import (
+	"log"
+
+	"github.com/Kosk0l/TgBotConverter/config"
+	"github.com/Kosk0l/TgBotConverter/intrernal/handlers"
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type App struct { 
+}
+
+func NewApp(cfg *config.Config) () {
+	bot, err := telegram.NewBotAPI(cfg.App.TOKEN)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Настраиваем получение апдейтов
+	u := telegram.NewUpdate(0)
+	u.Timeout = 30
+
+	// канал чтения из апи тг
+	updates := bot.GetUpdatesChan(u)
+
+	// проходка по каналу
+	for update := range updates {
+		handlers.HandleUpdate(bot, update)
+	}
+}
