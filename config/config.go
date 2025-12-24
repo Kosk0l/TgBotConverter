8d85804b9852ce388569468cfc0b4d39b354f4f5
Kosk0l <kosk0l@yandex.ru>
2025-12-24 17:38:34 +0300

    created config

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"log"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
+type DBConfig struct {
+	Host string `env:"DB_HOST" envDefault:"localhost"`
+	Port string `env:"DB_PORT" envDefault:"5432"`
+	User string `env:"DB_USER, required"`
+	Pass string `env:"DB_PASS, required"`
+	Name string `env:"DB_NAME, required"`
+	SSL  string `env:"DB_SSL" envDefault:"disable"`
+}
+
+type TGConfig struct {
+	TOKEN string `env:"TG_TOKEN,required"` // обязано существовать - Parse вернет ошибку
+}
+
+type Config struct {
+	Db 	DBConfig
+	App TGConfig
+}
+
+func Load() (*Config) {
+	_ = godotenv.Load()
+
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	return &cfg
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.2
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 
 require (
+	github.com/caarlos0/env/v11 v11.3.1
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/joho/godotenv v1.5.1
 )
